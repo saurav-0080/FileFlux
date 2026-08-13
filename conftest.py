0161d7747f8b086e605a969d89cc17d4c0dab2db
Saurav chaudhary <gc239436@gmail.com>
2026-08-13 16:19:35 +0530

    Skip GUI tests in CI, add conftest

---
+++ conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        if "test_gui" in str(item.fspath):
+            item.add_marker(pytest.mark.skip(reason="GUI tests require a display"))
