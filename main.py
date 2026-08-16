6e51a98b8abcd3d244ae3ef8d783ce4c5b893a9b
Saurav chaudhary <gc239436@gmail.com>
2026-08-16 23:00:24 +0530

    Fix lint: make main.py executable, ignore EXE001

--- main.py
+++ main.py
@@ -3,15 +3,18 @@
 FileFlux entry point.
 Launches CLI if arguments are provided, otherwise opens the GUI.
 """
+
 import sys
 
 
 def main():
     if len(sys.argv) > 1:
         from app.cli import run_cli
+
         run_cli()
     else:
         from app.gui.app import launch
+
         launch()
 
 
